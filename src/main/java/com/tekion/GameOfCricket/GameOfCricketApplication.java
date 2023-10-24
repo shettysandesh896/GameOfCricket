699e7512dfac973090b58da4fb08fb1601d22e6c
shettysandesh896 <148845278+shettysandesh896@users.noreply.github.com>
2023-10-24 17:50:24 +0530

    First Commit...

---
+++ src/main/java/com/tekion/GameOfCricket/GameOfCricketApplication.java
@@ -0,0 +1,14 @@
+package com.tekion.GameOfCricket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameOfCricketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameOfCricketApplication.class, args);
+		System.out.print("Sahshagsh");
+	}
+
+}
---
+++ src/test/java/com/tekion/GameOfCricket/GameOfCricketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tekion.GameOfCricket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameOfCricketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
